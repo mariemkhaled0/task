02b6f70cfaf4c62ebce0db616e02644e71d6233a
mariem <mariemkhaled834@gmail.com>
2024-11-01 14:47:56 +0200

    Updated animation for cards

--- main.js
+++ main.js
@@ -88,3 +88,12 @@ function animateNumber() {
 document.addEventListener("DOMContentLoaded", function () {
   animateNumber();
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.classList.add("show");
+    }, index * 200); // delay by 200ms for each card
+  });
+});
